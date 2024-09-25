0418acffe0e2be10bf17871b86e3481b6a30af87
Gabriel Nascimento <gabriel.nascimento@sao-ml-mq6kq2mc2f.wk.com>
2024-09-25 11:28:45 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleMode() {
+    const html = document.documentElement
+
+    // Esse é uma exemplo de como realizar a função de remover e adicionar//    
+    //if(html.classList.contains('light')) {
+    //    html.classList.remove('light')
+    //} else {
+    //    html.classList.add('light')
+    //}
+
+    html.classList.toggle('light')
+}
